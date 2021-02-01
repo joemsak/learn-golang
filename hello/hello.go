2369e104aa7a668778708e691717c9d42a1f33da
Joe Sak <joe@joesak.com>
2021-01-31 19:26:55 -0600

    Random greetings for multiple people, modified from the tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -11,11 +11,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
